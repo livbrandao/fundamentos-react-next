9851fac6a252ad5b3e63eb2b4cc7e8d34de9371b
livbrandao <liviatoledo27@gmail.com>
2025-02-17 21:34:24 -0300

    Updates

---
+++ _next/static/chunks/app/layout-635d8cd4d3d283bf.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{9049:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,347,23)),Promise.resolve().then(a.t.bind(a,877,23))},347:()=>{},877:e=>{e.exports={style:{fontFamily:"'Montserrat', 'Montserrat Fallback'",fontStyle:"normal"},className:"__className_69b34e"}}},e=>{var s=s=>e(e.s=s);e.O(0,[323,441,517,358],()=>s(9049)),_N_E=e.O()}]);
